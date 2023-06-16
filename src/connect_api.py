2ace151d4f6f173a8ef2e2ec101bc82d516f8820
Матвей <roganov94@mail.ru>
2023-06-17 01:17:11 +0300

    Создал два класса для получения вакансий с HH и SuperJob

--- src/connect_api.py
+++ src/connect_api.py
@@ -1,13 +1,43 @@
+import requests
+import os
 from abc import ABC, abstractmethod
 
 
 class ConnectAPI(ABC):
 
     @abstractmethod
-    def connect_api(self):
+    def get_vacancies(self, search: str):
         pass
 
-    @abstractmethod
-    def get_vacancies(self):
-        pass
+
+class HeadHunterAPI(ConnectAPI):
+
+    def __init__(self, url: str = "https://api.hh.ru/vacancies"):
+        self.url = url
+
+    def get_vacancies(self, search):
+        params = {"text": search}
+        response = requests.get(self.url, params=params).json()
+        return response
+
+
+class SuperJobAPI(ConnectAPI):
+
+    def __init__(self, url: str = "https://api.superjob.ru/2.0/vacancies/"):
+        self.url = url
+
+    def get_vacancies(self, search):
+        api_super_job = os.getenv('TOKEN_API_SUPERJOB')
+        headers = {"X-Api-App-Id": api_super_job}
+        params = {
+            'POST': '/2.0/vacancies/',
+            'Host': 'api.superjob.ru',
+            'Authorization': 'Bearer r.000000010000001.example.access_token',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'keywords': search
+        }
+
+        response = requests.get(self.url, headers=headers, params=params).json()
+        return response
+
 
